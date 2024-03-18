c4680bc06c18563cd2be53fd54ee15f20afc787d
Andrey <fun-chel@list.ru>
2024-03-19 02:25:26 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <MgfsoMossportRuByHtmlToDesignFreeVersion1903202413602Gmt3  />
+</template>
+
+<script>
+import MgfsoMossportRuByHtmlToDesignFreeVersion1903202413602Gmt3 from "./MgfsoMossportRuByHtmlToDesignFreeVersion1903202413602Gmt3/MgfsoMossportRuByHtmlToDesignFreeVersion1903202413602Gmt3.vue";
+import "./vars.css";
+import "./styles.css";
+
+export default {
+  name: "App",
+  components: {
+    MgfsoMossportRuByHtmlToDesignFreeVersion1903202413602Gmt3: MgfsoMossportRuByHtmlToDesignFreeVersion1903202413602Gmt3,
+  },
+};
+</script>
+
+<style>
+</style>
+
